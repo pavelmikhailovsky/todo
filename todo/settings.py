eecd3da0df3a5c857b92a5394ced87ecff418664
pavelmikhailovsky <dsvknsddfasfawe@gmail.com>
2021-07-12 12:11:46 +0300

    update rest_framework settings

--- todo/settings.py
+++ todo/settings.py
@@ -142,7 +142,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
 
 SIMPLE_JWT = {
