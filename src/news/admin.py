ce4a35f444aa6a343ff6e0ee7b9b822860e771ff
pavelmikhailovsky <dsvknsddfasfawe@gmail.com>
2021-08-07 23:48:10 +0300

    little modified

--- src/news/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import News
-
-
-@admin.register(News)
-class NewsAdmin(admin.ModelAdmin):
-    pass
--- src/news/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import News
-
-
-class NewsSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = News
-        fields = ['name', 'description', 'create_at']
