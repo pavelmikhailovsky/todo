7b8b4239227c24d553847c7e7fa95c2c155f496f
pavelmikhailovsky <dsvknsddfasfawe@gmail.com>
2021-08-07 23:46:45 +0300

    modified

--- todo/settings.py
+++ todo/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     # app
-    'src.news',
     'src.notes',
     
     # utils
@@ -85,8 +84,12 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Todo_list',
+        'USER': 'postgres',
+        'PASSWORD': 'pavel',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
