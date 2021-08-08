c227e4d73e5c5e18fa55aa65c996cbe9672b5aaa
pavelmikhailovsky <dsvknsddfasfawe@gmail.com>
2021-08-08 18:39:11 +0300

    create new tests for views api and notes model

--- src/notes/tests.py
+++ src/notes/tests.py
@@ -1,17 +1,43 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
 
-from rest_framework.test import APIClient, force_authenticate
+from rest_framework.test import APITestCase
+from rest_framework import status
+
+from .models import Notes
 
 
-# TODO НАПИСАТЬ НОРМ ТЕСТЫ!!!!11!!!!1111!!1
-class NotesTests(TestCase):
-    def test_view(self):
-        client = APIClient()
-        User.objects.create(username='test', password='test', email='')
-        user = User.objects.get(username='test')
-        request = client.post('http://127.0.0.1:8000/api/v1.0/notes/', {'name': 'wwww', 'text': 'text'}, format='json')
-        force_authenticate(request, user=user, token=user.auth_token)
-        
-        self.assertEqual(request.status_code, 201)
-    
+class NotesViewAPITests(APITestCase):
+    """ Tests views API """
+    def notes_api_test(self, url):
+        response_get = self.client.get(url)
+        response_post_no = self.client.post(url, {})
+        self.assertEqual(response_get.status_code, status.HTTP_200_OK)
+        self.assertEqual(response_post_no.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_api_notes_list(self):
+        url = '/api/v1.0/notes/'
+        response_post_ok = self.client.post(url, {'text': 'test text'})
+        self.notes_api_test(url)
+        self.assertEqual(response_post_ok.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Notes.objects.get().text, 'test text')
+        self.assertEqual(Notes.objects.count(), 1)
+
+    def test_api_notes_detail(self):
+        Notes.objects.create(text='test')
+        url = '/api/v1.0/notes/1/'
+        response_post_ok = self.client.post(url, {'text': 'test text'})
+        self.notes_api_test(url)
+        response_delete = self.client.delete(url)
+        self.assertEqual(response_post_ok.status_code, status.HTTP_202_ACCEPTED)
+        self.assertEqual(response_delete.status_code, status.HTTP_204_NO_CONTENT)
+
+
+class NotesModelTest(TestCase):
+    def test_update_at_field(self):
+        notes = Notes.objects.create(text='Test notes')
+        new_notes = Notes.objects.get(id=1)
+        new_notes.text = 'Notes test'
+        new_notes.save()
+        self.assertEqual(notes.update_at, 'Не редактирован')
+        self.assertNotEqual(new_notes.update_at, 'Не редактирован')
+
