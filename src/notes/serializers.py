cff7ec8f5f364af7724fd7284dd4ad07d9a4637f
pavelmikhailovsky <dsvknsddfasfawe@gmail.com>
2021-08-11 21:11:58 +0300

    minor edits old serializer and create new

--- src/notes/serializers.py
+++ src/notes/serializers.py
@@ -1,22 +1,26 @@
-from django.contrib.auth.models import User
-
 from rest_framework import serializers
 
 from .models import Notes
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['username', 'email']
+from ..users.serializers import UserSerializer
 
 
 class NotesSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True, required=False)
     create_at = serializers.CharField(read_only=True, required=False)
     update_at = serializers.CharField(read_only=True, required=False)
-    user = UserSerializer(required=False)
+    user = UserSerializer(read_only=True, required=False)
 
     class Meta:
         model = Notes
+        ref_name = 'notes'  # for swagger
         fields = ['id', 'text', 'create_at', 'update_at', 'user']
+
+
+class AllNotesSerializer(serializers.Serializer):
+    count = serializers.IntegerField(read_only=True)
+
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
